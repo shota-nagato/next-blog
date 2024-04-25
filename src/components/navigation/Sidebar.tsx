f29c4b20b278a24fb16cec3bb4279a91d62ffc1a
shota-nagato <shota.nagato@gmail.com>
2024-04-25 20:44:41 +0900

    文字色修正

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -3,14 +3,14 @@ export const Sidebar = () => {
     <aside className="w-full md:w-1/3 container mx-auto">
       <div className="px-4 bg-white shadow rounded-lg py-4">
         <h2 className="text-sm font-bold">ここに名前が入ります</h2>
-        <p className="text-xs text-gray-400">webエンジニア</p>
+        <p className="text-xs text-gray-700">webエンジニア</p>
         <p className="text-xs mt-2">Railsとか</p>
       </div>
       <div className="bg-white shadow rounded-lg pb-4 mt-4">
         <h2 className="px-4 py-2 text-sm rounded-t-lg font-bold bg-[#efefef]">
           人気の記事
         </h2>
-        <ul className="flex flex-col gap-2 mt-4 px-4 text-sm text-gray-500">
+        <ul className="flex flex-col gap-2 mt-4 px-4 text-sm text-gray-700">
           <li>Rails</li>
           <li>CSS</li>
         </ul>
@@ -19,7 +19,7 @@ export const Sidebar = () => {
         <h2 className="px-4 py-2 text-sm rounded-t-lg font-bold bg-[#efefef]">
           カテゴリー
         </h2>
-        <ul className="flex flex-col gap-2 mt-4 px-4 text-sm text-gray-500">
+        <ul className="flex flex-col gap-2 mt-4 px-4 text-sm text-gray-700">
           <li>Rails</li>
           <li>CSS</li>
         </ul>
