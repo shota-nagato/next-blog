61d179110434db2b663bad527000881b6c7c2f53
shota-nagato <shota.nagato@gmail.com>
2024-05-05 11:16:25 +0900

    自動フォーマット設定

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -1,28 +1,30 @@
-import Categories from '@/components/Categories'
-import { getCategories } from '@/libs/microcms'
 import { use } from 'react'
+
+import Categories from '@/components/Categories'
+
+import { getCategories } from '@/libs/microcms'
 
 export const Sidebar = () => {
   const { contents } = use(getCategories({ limit: 50 }))
 
   return (
-    <aside className="w-full md:w-1/3 container mx-auto">
-      <div className="px-4 bg-white shadow rounded-lg py-4">
+    <aside className="container mx-auto w-full md:w-1/3">
+      <div className="rounded-lg bg-white p-4 shadow">
         <h2 className="text-sm font-bold">ここに名前が入ります</h2>
         <p className="text-xs text-gray-700">webエンジニア</p>
-        <p className="text-xs mt-2">Railsとか</p>
+        <p className="mt-2 text-xs">Railsとか</p>
       </div>
-      <div className="bg-white shadow rounded-lg pb-4 mt-4">
-        <h2 className="px-4 py-2 text-sm rounded-t-lg font-bold bg-[#efefef]">
+      <div className="mt-4 rounded-lg bg-white pb-4 shadow">
+        <h2 className="rounded-t-lg bg-[#efefef] px-4 py-2 text-sm font-bold">
           人気の記事
         </h2>
-        <ul className="flex flex-col gap-2 mt-4 px-4 text-sm text-gray-700">
+        <ul className="mt-4 flex flex-col gap-2 px-4 text-sm text-gray-700">
           <li>Rails</li>
           <li>CSS</li>
         </ul>
       </div>
-      <div className="bg-white shadow rounded-lg pb-4 mt-4">
-        <h2 className="px-4 py-2 text-sm rounded-t-lg font-bold bg-[#efefef]">
+      <div className="mt-4 rounded-lg bg-white pb-4 shadow">
+        <h2 className="rounded-t-lg bg-[#efefef] px-4 py-2 text-sm font-bold">
           カテゴリー
         </h2>
         <Categories categories={contents} />
