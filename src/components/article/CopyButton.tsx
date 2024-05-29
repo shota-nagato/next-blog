07d0a701ed73908689e793c4206028b4397bad7d
shota-nagato <shota.nagato@gmail.com>
2024-05-29 23:20:52 +0900

    不要

--- src/components/article/CopyButton.tsx
+++ src/components/article/CopyButton.tsx
@@ -1,29 +1,29 @@
-'use client'
-import Image from 'next/image'
+// 'use client'
+// import Image from 'next/image'
 
-type Props = {
-  url: string
-}
+// type Props = {
+//   url: string
+// }
 
-export const CopyButton = (props: Props) => {
-  const copyLink = async (): Promise<void> => {
-    try {
-      await navigator.clipboard.writeText(props.url)
-      alert('クリップボードにコピーしました')
-    } catch (error) {
-      alert('失敗しました')
-    }
-  }
-  return (
-    <div>
-      <button
-        className="flex items-center gap-[10px]"
-        onClick={() => copyLink()}
-        type="button"
-      >
-        <Image alt="linkアイコン" height={30} src="/link.svg" width={30} />
-        <div className="text-[14px] text-secondary-black">url copy</div>
-      </button>
-    </div>
-  )
-}
+// export const CopyButton = (props: Props) => {
+//   const copyLink = async (): Promise<void> => {
+//     try {
+//       await navigator.clipboard.writeText(props.url)
+//       alert('クリップボードにコピーしました')
+//     } catch (error) {
+//       alert('失敗しました')
+//     }
+//   }
+//   return (
+//     <div>
+//       <button
+//         className="flex items-center gap-[10px]"
+//         onClick={() => copyLink()}
+//         type="button"
+//       >
+//         <Image alt="linkアイコン" height={30} src="/link.svg" width={30} />
+//         <div className="text-[14px] text-secondary-black">url copy</div>
+//       </button>
+//     </div>
+//   )
+// }
