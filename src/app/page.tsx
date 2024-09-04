b63d0a1365556262351e16a8828cd9c2ec2bfd7d
shota-nagato <shota.nagato@gmail.com>
2024-09-04 21:14:46 +0900

    記事取得の上限数を100に変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ArticleCardList from '@/components/article/CardList'
 import { getArticles } from '@/libs/microcms'
 
 export default function Home() {
-  const { contents } = use(getArticles())
+  const { contents } = use(getArticles({ limit: 100 }))
 
   return (
     <Suspense fallback={'Loading....'}>
